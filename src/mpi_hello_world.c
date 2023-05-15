cb356f8a4159f83ff39421133b2edc56a14be525
Blake Devcich <blake.devcich@hpe.com>
2023-05-15 14:09:49 -0500

    add secondary storage
    Signed-off-by: Blake Devcich <blake.devcich@hpe.com>

--- src/mpi_hello_world.c
+++ src/mpi_hello_world.c
@@ -19,6 +19,7 @@
 int main(int argc, char **argv)
 {
   char nnf_storage_path[PATH_MAX];
+  char nnf_storage_path2[PATH_MAX];
 
   // Initialize the MPI environment. The two arguments to MPI Init are not
   // currently used by MPI implementations, but are there in case future
@@ -38,12 +39,13 @@ int main(int argc, char **argv)
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
 
-  if (argc < 2)
+  if (argc < 3)
   {
-    printf("Storage parameter not supplied\n");
+    printf("Storage parameters not supplied\n");
     return -1;
   }
   strncpy(nnf_storage_path, argv[1], PATH_MAX);
+  strncpy(nnf_storage_path2, argv[2], PATH_MAX);
 
   // Print off a hello world message
   char hostname[1024];
@@ -53,30 +55,53 @@ int main(int argc, char **argv)
          processor_name, world_rank, world_size, nnf_storage_path, hostname);
 
   // We're using a GFS2 filesystem, which has index mounts for every compute node
-  // e.g. /mnt/nnf/5d335081-cd0f-4b8a-a1f4-94860a8ae702-0/0/
+  // e.g. /mnt/nnf/5d335081-cd0f-4b8a-a1f4-94860a8ae702-1/0/
   if (sprintf(nnf_storage_path, "%s/0/testfile", nnf_storage_path) == -1)
   {
     fprintf(stderr, "rank %d: %s\n", world_rank, strerror(errno));
     return errno;
   }
-
   printf("rank %d: test file: %s\n", world_rank, nnf_storage_path);
 
+  // Lustre does not use index mounts
+  // e.g. /mnt/nnf/5d335081-cd0f-4b8a-a1f4-94860a8ae702-0/
+  if (sprintf(nnf_storage_path2, "%s/testfile", nnf_storage_path2) == -1)
+  {
+    fprintf(stderr, "rank %d: %s\n", world_rank, strerror(errno));
+    return errno;
+  }
+  printf("rank %d: test file2: %s\n", world_rank, nnf_storage_path2);
+
+  // Open a testfile on each filesystem
   int fd = open(nnf_storage_path, O_WRONLY | O_CREAT);
   if (fd == -1)
   {
     fprintf(stderr, "rank %d: error opening file: %s\n", world_rank, strerror(errno));
     return errno;
   }
+  int fd2 = open(nnf_storage_path2, O_WRONLY | O_CREAT);
+  if (fd2 == -1)
+  {
+    fprintf(stderr, "rank %d: error opening file2: %s\n", world_rank, strerror(errno));
+    return errno;
+  }
 
+  // Allocate some storage for each test file
   int res = posix_fallocate(fd, 0, 100);
   if (res == -1)
   {
     fprintf(stderr, "rank %d: error allocating file: %s\n", world_rank, strerror(errno));
     return errno;
   }
+  int res2 = posix_fallocate(fd2, 0, 100);
+  if (res2 == -1)
+  {
+    fprintf(stderr, "rank %d: error allocating file2: %s\n", world_rank, strerror(errno));
+    return errno;
+  }
 
   printf("rank %d: wrote file to '%s'\n", world_rank, nnf_storage_path);
+  printf("rank %d: wrote file2 to '%s'\n", world_rank, nnf_storage_path2);
 
   // Finalize the MPI environment. No more MPI calls can be made after this
   MPI_Finalize();
