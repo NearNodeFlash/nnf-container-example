4de4d5e4689c2ee0ed09d59e45933265ed50b6c2
Dean Roehrich <dean.roehrich@hpe.com>
2024-10-07 12:12:24 -0500

    Update to match latest DWS/NNF
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- src/mpi_hello_world.c
+++ src/mpi_hello_world.c
@@ -7,10 +7,12 @@
 // An intro MPI hello world program that uses MPI_Init, MPI_Comm_size,
 // MPI_Comm_rank, MPI_Finalize, and MPI_Get_processor_name.
 //
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -44,6 +46,8 @@ int main(int argc, char **argv)
     return -1;
   }
   strncpy(nnf_storage_path, argv[1], PATH_MAX);
+  // A production app would verify that it fits.
+  nnf_storage_path[PATH_MAX-1] = '\0';
 
   // Print off a hello world message
   char hostname[1024];
@@ -53,8 +57,36 @@ int main(int argc, char **argv)
          processor_name, world_rank, world_size, nnf_storage_path, hostname);
 
   // We're using a GFS2 filesystem, which has index mounts for every compute node
-  // e.g. /mnt/nnf/5d335081-cd0f-4b8a-a1f4-94860a8ae702-0/0/
-  if (sprintf(nnf_storage_path, "%s/0/testfile", nnf_storage_path) == -1)
+  // e.g. /mnt/nnf/5d335081-cd0f-4b8a-a1f4-94860a8ae702-0/rabbit-node-1-0/
+
+  DIR *dir;
+  struct dirent *entry, *lentry;
+  char indexed_dir[PATH_MAX];
+
+  // Pick one of the indexed directories in the chosen storage. The directories
+  // within the chosen storage are indexed, with one for each compute that has
+  // access to this storage.
+  // For this demo, we'll pick the last indexed dir. A full-scale app would
+  // work through each of the indexed dirs.
+  dir = opendir(nnf_storage_path);
+  if (dir == NULL)
+  {
+    perror("opendir");
+    return -1;
+  }
+  while ((entry = readdir(dir)) != NULL)
+  {
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+      continue;
+    printf("Found indexed dir: %s/%s\n", nnf_storage_path, entry->d_name);
+    // This demo uses only the last indexed dir.
+    lentry = entry;
+  }
+  // In production, we would verify that it actually fits.
+  sprintf(indexed_dir, "%s/%s", nnf_storage_path, lentry->d_name);
+  closedir(dir);
+
+  if (sprintf(nnf_storage_path, "%s/testfile", indexed_dir) == -1)
   {
     fprintf(stderr, "rank %d: %s\n", world_rank, strerror(errno));
     return errno;
